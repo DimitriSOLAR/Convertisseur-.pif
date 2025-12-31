76c28d6e78b978ae4412c0b5d943a7dff2553052
loisonv <solardimitri@gmail.com>
2025-12-31 15:01:07 +0100

    Javadoc avancement

--- src/pif/ActionConvertir.java
+++ src/pif/ActionConvertir.java
@@ -3,6 +3,11 @@ package pif;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * @author Dimitri SOLAR, Valentin LOISON
+ * @version 1.0
+ **/
+
 public class ActionConvertir implements ActionListener {
     private FenetreConvertisseur fenetre;
 
--- src/pif/ActionOuvrir.java
+++ src/pif/ActionOuvrir.java
@@ -3,6 +3,11 @@ package pif;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * @Author Dimitri SOLAR, Valentin LOISON
+ * @version 1.0
+ * */
+
 public class ActionOuvrir implements ActionListener {
     private FenetreConvertisseur fenetre;
 
